2963198fec83a78e6e93d275fc038b8e54b551cd
Arthur <angloong@outlook.com>
2021-07-20 08:26:38 +0800

    v1.1 2021/07/20 08:25 - 修改串口文件，可以收发16进制命令。

--- serial_test.py
+++ serial_test.py
@@ -1,12 +1,11 @@
 #!python3
+#v0.1.0
 
-from pprint import pprint
-import sys
-
-pprint(sys.path)
+""""
+    学习python 串口通讯的程序
+"""
 
 import serial
-
 import serial.tools.list_ports
 
 port_list = list(serial.tools.list_ports.comports())
@@ -17,3 +16,24 @@ if len(port_list) == 0:
 else:
     for i in range(0,len(port_list)):
         print(port_list[i])
+
+try:
+    portx = "COM6"
+    bps = 115200
+    timex = None #None表示永远等待，单位为秒
+    ser = serial.Serial(portx,bps,timeout=timex)
+    print("串口参数详情：",ser)
+
+    result = ser.write(bytes(b'\xee\xb1\x00\x00\x06\xff\xfc\xff\xff'))
+
+    print("写字数",result)
+
+    for count in range(5):
+        print(ser.read().hex())
+
+    print("-------------")
+    ser.close()
+
+except Exception as e:
+    print("error:",e)
+
