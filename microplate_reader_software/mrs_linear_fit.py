97c280526418673f1ad1499db5696c4432955a63
Arthur <angloong@outlook.com>
2021-07-21 09:30:21 +0800

    v1.4 2021/07/21 09:26 - 将mrs_leastql.py 更名为mrs_linear_fit.py - 将mrs_linear_fit.py中增加另一种实现线性拟合的方法，并计算r和R2.

--- microplate_reader_software/mrs_linear_fit.py
+++ microplate_reader_software/mrs_linear_fit.py
@@ -27,10 +27,30 @@ def function_get_liner_exp(x, y):
     return poly1d(fun_para)
 
 
+def function_rsquared(x, y):
+    """
+    进行线性拟合，并计算R2.
+
+    Args:
+        x,x轴数据列表
+        y,y轴数据列表
+
+    Returns:
+        计算得到的方程k,b,r,R2
+
+    Raises:
+
+    """
+    slope,intercept,r_value,p_value,std_err = stats.linregress(x,y)
+    return (slope,intercept,r_value,r_value**2)
+
+
 x = [1,2,3,4,5]
 y = [0.8,1.9,3.0,3.8,5.1]
 fun_exp = function_get_liner_exp(x,y)
 print(fun_exp)
+fun_exp2 = function_rsquared(x,y)
+print("fun_exp2:y=",fun_exp2[0],"*x+",fun_exp2[1],"   r=",fun_exp2[2],"   R2=",fun_exp2[3])
 line_x = [1,5]
 line_y = [fun_exp(1),fun_exp(5)]
 plt.scatter(x,y)
